ce84d87f87cdd65759abfbf3005eef9154a266c1
Marcin Witkowski (Laptop) <witkow@agh.edu.pl>
2024-01-10 17:56:26 +0100

    BCE loss, sigmoid and linear activation functions, plotting

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,14 +1,17 @@
+import math
+
 
 class Value:
     """ stores a single scalar value and its gradient """
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self.label = label
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -47,6 +50,39 @@ class Value:
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def sigmoid(self):
+        out = Value(1 / (1 + math.exp(-1 * self.data)), (self,), 'Sigmoid')
+
+        def _backward():
+            self.grad += (out.data * (1 - out.data)) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def linear(self):
+        out = Value(self.data, (self,), 'Linear')
+
+        def _backward():
+            self.grad += out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def bce(self, true_val):
+        out = Value(-1 *
+                    (true_val.data * math.log(self.data) +
+                     (1 - true_val.data) * math.log((1 - self.data))
+                     ), (self,), 'BCE')
+
+        def _backward():
+            self.grad += ((self.data - true_val.data) /
+                          (self.data * (1-self.data)) * out.grad)
         out._backward = _backward
 
         return out
